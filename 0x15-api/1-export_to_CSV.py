378fae64b146a146eb8bbca63d25223bfc16c1b4
matxa <42968545+matxa@users.noreply.github.com>
2020-09-14 03:12:48 -0400

    fix documentation and finish task 0

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Using ReSTFull  Api to manipulate fake data"""
+"""Using ReSTFull  Api to manipulate fake data
+Exporting data to file in csv format
+"""
 import requests
 from sys import argv
 import json
+import csv
 
 
 """request user by id"""
 request_employee = requests.get(
     'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
 """json to dictionary"""
-employee = json.loads(request_employee.text)
-"""Get emplyee's name"""
-employee_name = employee.get("name")
+user = json.loads(request_employee.text)
+"""Get username"""
+username = user.get("username")
 
 
 """requesting all of users todo"""
@@ -20,20 +23,16 @@ request_todos = requests.get(
 """Dictionary to hold boolean value of completed tasks"""
 tasks = {}
 """json to list[dictionaries]"""
-employee_todos = json.loads(request_todos.text)
+user_todos = json.loads(request_todos.text)
 """loop through all the dictionary in
 the list and get bool value of key 'completed'
 """
-for dictionary in employee_todos:
+for dictionary in user_todos:
     tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
 
-"""Output name, done/total, then list done"""
-EMPLOYEE_NAME = employee_name
-TOTAL_NUMBER_OF_TASKS = len(tasks)
-NUMBER_OF_DONE_TASKS = len([k for k, v in tasks.items() if v is True])
-print("Employee {} is done with tasks({}/{}):".format(
-    EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-for k, v in tasks.items():
-    if v is True:
-        print('\t' + k)
+"""export data to file cvs format"""
+with open('{}.csv'.format(argv[1]), mode='w') as file:
+    file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+    for k, v in tasks.items():
+        file_editor.writerow([argv[1], username, v, k])
