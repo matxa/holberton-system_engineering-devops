f55781878305c12dcd6d17b01b127386b1bf873a
matxa <42968545+matxa@users.noreply.github.com>
2020-09-14 21:28:38 -0400

    del doc

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,32 +2,22 @@
 """Using ReSTFull  Api to manipulate fake data
 Exporting data to file in json format
 """
-import json
-import requests
-
 
 if __name__ == "__main__":
-    """dict to dict"""
+    import json
+    import requests
+
     list_of_users = {}
     for i in range(1, 11):
-        """request user by id"""
         request_employee = requests.get(
             'https://jsonplaceholder.typicode.com/users/{}/'.format(i))
-        """json to dictionary"""
         user = json.loads(request_employee.text)
-        """Get username"""
         username = user.get("username")
 
-        """requesting all of users todo"""
         request_todos = requests.get(
             'https://jsonplaceholder.typicode.com/users/{}/todos'.format(i))
-        """Dictionary to hold boolean value of completed tasks"""
         tasks = {}
-        """json to list[dictionaries]"""
         user_todos = request_todos.json()
-        """loop through all the dictionary in
-        the list and get bool value of key 'completed'
-        """
         for dictionary in user_todos:
             tasks.update(
                 {dictionary.get("title"): dictionary.get("completed")})
@@ -41,6 +31,5 @@ if __name__ == "__main__":
             })
         list_of_users[i] = task_list
 
-    """export data to file json format"""
     with open('todo_all_employees.json', mode='w') as file:
         json.dump(list_of_users, file)
