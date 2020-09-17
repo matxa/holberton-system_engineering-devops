b793ecc9e098a682acb67f1f7e774ed4ec199054
matxa <42968545+matxa@users.noreply.github.com>
2020-09-17 14:20:15 -0400

    add user-agent to header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,17 @@ import requests
 def number_of_subscribers(subreddit):
     """request to get number of subs
     """
+    headers = {"User-Agent": "MakingTheCheckerHappy"}
+
     response = requests.get(
         'https://www.reddit.com/r/{}/about.json'.format(subreddit),
-        allow_redirects=False)
+        allow_redirects=False,
+        headers=headers)
+
     if response.status_code == 200:
         json_data = response.json()
         data = json_data.get('data')
         subs = data.get('subscribers')
         return subs
+
     return 0
