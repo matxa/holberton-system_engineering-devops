b0aba6febe87fd81d430ff6f30ab71e672363a3c
matxa <42968545+matxa@users.noreply.github.com>
2020-09-30 18:58:41 -0400

    puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,1 @@
+exec { '/var/www/html/wp-setting.php':  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],  command => sed -i s/.phpp/.php/g /var/www/html/wp-settings.php,}
