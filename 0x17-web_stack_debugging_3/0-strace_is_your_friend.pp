879b3b3b3bed2589a3a040417613a2d2dbf4ca5c
matxa <42968545+matxa@users.noreply.github.com>
2020-09-30 23:27:28 -0400

    fix file error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,17 @@
-exec { '/var/www/html/wp-setting.php':  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],  command => sed -i s/.phpp/.php/g /var/www/html/wp-settings.php,}
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
+}
+
+package { 'apache2':
+  require => Exec['apt-update'],
+  ensure => installed,
+}
+
+service { 'apache2':
+  ensure => running,
+}
+
+exec { '/var/www/html/wp-setting.php':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+}
