1ea6c3a4b9f1a03fb47e61af58742f5a448431c2
matxa <42968545+matxa@users.noreply.github.com>
2020-10-08 21:46:43 -0400

    use sed instead of change the intire files content

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 #Fixing nginx ULIMIT
 
 exec { 'replace_limit':
-  command => 'sed -i "/ULIMIT=/c\ULIMIT=\'-n 4096\'" /etc/default/nginx',
   path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "/ULIMIT=/c\ULIMIT=\'-n 4096\'" /etc/default/nginx',
 }
 
 exec { 'nginx_restart':
+  path    => '/usr/bin:/usr/sbin:/bin',
   command => 'sudo service nginx restart',
-  path    => '/usr/bin/',
 }
 
