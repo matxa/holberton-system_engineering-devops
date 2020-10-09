27ead07bdd293342e81ac421aa1091aaa9c1ad12
matxa <42968545+matxa@users.noreply.github.com>
2020-10-08 21:55:19 -0400

    huh

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,6 @@
 
 exec { 'replace_limit':
   path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'sed -i "/ULIMIT=/c\ULIMIT=\'-n 4096\'" /etc/default/nginx',
-}
-
-exec { 'nginx_restart':
-  path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'service nginx restart',
+  command => 'sed -i "/ULIMIT=/c\ULIMIT=\'-n 4096\'" /etc/default/nginx; service nginx restart',
 }
 
